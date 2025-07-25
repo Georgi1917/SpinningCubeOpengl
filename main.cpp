42ce0bb32e279f209ce5edf51b9004fb5ae53fc3
Gosho Zvqr <mamarida15@abv.bg>
2025-07-25 19:44:44 +0300

    added a rotating triangle along the z axis

--- main.cpp
+++ main.cpp
@@ -1,7 +1,58 @@
 #define GLEW_STATIC
 #include "include/glew.h"
 #include "include/glfw3.h"
+#include "include/glm/glm.hpp"
+#include "include/glm/gtc/matrix_transform.hpp"
+#include "include/glm/gtc/type_ptr.hpp"
 #include <iostream>
+#include <sstream>
+#include <fstream>
+
+std::string GetShader(std::string &filepath)
+{
+
+    std::ifstream f(filepath);
+    std::string line;
+    std::stringstream s;
+    
+    while(std::getline(f, line))
+    {
+        s << line << "\n";
+    }
+
+    std::cout << s.str() << "\n";
+
+    return s.str();
+
+}
+
+unsigned int CreateShaders(std::string &vsSource, std::string &fsSource)
+{
+
+    unsigned int vertex, fragment, program;
+    vertex = glCreateShader(GL_VERTEX_SHADER);
+    fragment = glCreateShader(GL_FRAGMENT_SHADER);
+
+    const char *vsrc = vsSource.c_str();
+    const char *fsrc = fsSource.c_str();
+
+    glShaderSource(vertex, 1, &vsrc, nullptr);
+    glShaderSource(fragment, 1, &fsrc, nullptr);
+
+    glCompileShader(vertex);
+    glCompileShader(fragment);
+
+    program = glCreateProgram();
+    glAttachShader(program, vertex);
+    glAttachShader(program, fragment);
+    glLinkProgram(program);
+
+    glDeleteShader(vertex);
+    glDeleteShader(fragment);
+
+    return program;
+
+}
 
 int main()
 {
@@ -39,12 +90,34 @@ int main()
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
     glEnableVertexAttribArray(0);
 
+    std::string vsFile = "shaders/vertex.shader";
+    std::string fsFile = "shaders/fragment.shader";
+
+    std::string vs = GetShader(vsFile);
+    std::string fs = GetShader(fsFile);
+
+    unsigned int program = CreateShaders(vs, fs);
+    glUseProgram(program);
+
+    glm::mat4 trans = glm::mat4(1.0f);
+
+    float currTime = glfwGetTime();
+    float lastTime = currTime;
+    float deltaTime;
+
     while(!glfwWindowShouldClose(window))
     {
 
-        //std::cout << "Hellooo" << "\n";
+        currTime = glfwGetTime();
+        deltaTime = currTime - lastTime;
+        lastTime = currTime;
 
         glClear(GL_COLOR_BUFFER_BIT);
+
+        trans = glm::rotate(trans, deltaTime, glm::vec3(0.0f, 0.0f, 1.0f));
+
+        unsigned int id = glGetUniformLocation(program, "u_Trans");
+        glUniformMatrix4fv(id, 1, GL_FALSE, glm::value_ptr(trans));
 
         glDrawArrays(GL_TRIANGLES, 0, 6);
 
---
+++ shaders/vertex.shader
@@ -0,0 +1,9 @@
+#version 330 core
+layout (location = 0) in vec3 pos;
+
+uniform mat4 u_Trans;
+
+void main()
+{
+    gl_Position = u_Trans * vec4(pos, 1.0);
+}
